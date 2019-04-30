5b046327885eef63345eedd613891882b8a8446b
Chi Tran <chidtran@gmail.com>
2019-04-30 16:50:58 -0600

    Adds invoice validations and relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,12 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :status 
+
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+
+  belongs_to :merchant
+  belongs_to :customer
+
+  enum status:['unshipped', 'shipped']
 end
